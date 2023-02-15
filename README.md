7f7baf84d939808d4ac55a1e5cd303dfe1ca1059
afolabiopakunle <opakunleafolabi@gmail.com>
2023-02-15 18:08:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ngrx-001
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-p2jlg7)
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+
+import { AppComponent } from './app.component';
+import { MyCounterComponent } from './my-counter/my-counter.component';
+import { counterReducer } from './state/counter.reducer';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({count: counterReducer})],
+  declarations: [AppComponent, MyCounterComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+
+/*
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://github.com/ngrx/platform
+*/
